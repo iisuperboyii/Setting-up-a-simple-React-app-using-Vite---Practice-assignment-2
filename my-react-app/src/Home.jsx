1aa51386ec60bec57548013169d1f1c124ed1cab
iiuperboyii <aaronjomon24@gmail.com>
2025-02-11 09:50:55 +0530

    completed required task

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Aaron Jomon</h1>
+            <p>Contact Details: aaron.jomon.s66@kalvium.community</p>
+        </div>
+    );
+};
+
+export default Home;
